cd29ce3516cf5e293920cebc856e2053778a56bd
Yogan JP <jp.yoganand@gmail.com>
2025-06-09 14:17:47 +0800

    Your clear, cute message

--- brazilian_ecommerce_team_3/models/dim_time.sql
+++ brazilian_ecommerce_team_3/models/dim_time.sql
@@ -1,45 +1,28 @@
 with times as (
-
-    -- Generate distinct timestamp columns from orders
-    select distinct
-        order_purchase_timestamp as time
+    select distinct cast(order_purchase_timestamp as timestamp) as time
     from {{ source('BET_Team3', 'orders') }}
-
     union distinct
-
-    select distinct
-        order_approved_at as time
+    select distinct cast(order_approved_at as timestamp) as time
     from {{ source('BET_Team3', 'orders') }}
-
     union distinct
-
-    select distinct
-        order_delivered_carrier_date as time
+    select distinct cast(order_delivered_carrier_date as timestamp) as time
     from {{ source('BET_Team3', 'orders') }}
-
     union distinct
-
-    select distinct
-        order_delivered_customer_date as time
+    select distinct cast(order_delivered_customer_date as timestamp) as time
     from {{ source('BET_Team3', 'orders') }}
-
     union distinct
-
-    select distinct
-        order_estimated_delivery_date as time
+    select distinct cast(order_estimated_delivery_date as timestamp) as time
     from {{ source('BET_Team3', 'orders') }}
-
 ),
-
 final as (
     select
         time as time_id,
         extract(hour from time) as hour,
         extract(minute from time) as minute,
-        format_time('%H:%M', time) as time_hhmm
+        format_time('%H:%M', cast(time as TIME)) as time_hhmm
     from times
     where time is not null
 )
-
 select * from final
+
 
